7854cf9f3718901f470d643a0e616a18c901ee7c
dinoalfiano <101788917+dinoalfiano@users.noreply.github.com>
2022-07-15 10:37:07 +0700

    Create mahasiswa.cs

---
+++ mahasiswa.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+
+        public int NIM { get; set; }
+        public string Nama { get; set; }
+        public string jenisKelamin { get; set; }
+        public double Ipk { get; set; }
+
+    }
+}
